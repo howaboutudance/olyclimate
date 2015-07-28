c50d40f018aaa3458ec61e0dcde0de444b52d40b
howaboutudance <howaboutudance@gmail.com>
2015-07-27 20:49:48 -0700

    started basics of sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
 	root 'users#new'
 	get 'home' => 'signup'
 	get 'signup' => 'users#new'
 	get 'error' => 'static_pages#error'
+	
+	#sessions
+	get 'login' => 'sessions#new'
+	post 'login' => 'sessions#create'
+	delete 'logout' => 'sessions#destroy'
 
   # resources
 	resources :users
