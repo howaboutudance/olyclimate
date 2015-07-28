682fd040a83b8709daafc4e36860b0be35f26782
howaboutudance <howaboutudance@gmail.com>
2015-07-27 21:16:33 -0700

    started basic function for handling login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,13 @@ class SessionsController < ApplicationController
   def new
   end
 	def create
-		render 'new'
+		user = User.find_by(email: params[:session][:email].downcase)
+		if user&&user.authenticate(params[:session][:password])
+			#TODO: fill in section with login logic
+		else
+			flash[:danger] = 'Invalid email/password combination'
+			render 'new'
+		end
 	end
 	def destory
 	end
