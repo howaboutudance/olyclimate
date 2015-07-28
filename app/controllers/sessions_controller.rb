26e2593bc709bf34aac1e135f98f0c64c80228e0
howaboutudance <howaboutudance@gmail.com>
2015-07-28 15:52:38 -0700

    fixed view to properly show current user on login page, added helper to session_helper since that is where view helpers belong

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,13 @@
 class SessionsController < ApplicationController
-	helper_method :current_user
   def new
   end
 	def create
 		user = AdminUser.from_omniauth(env['omniauth.auth'])
-		session[:user_id] = user.id
+		session[:AdminUser_id] = user.id
 		redirect_to login_path
 	end
-	def destory
-		session[:user_id] = nil
+	def destroy
+		session[:AdminUser_id] = nil
 		redirect_to login_path
-	end
-	def current_user
-		@current_user ||= AdminUser.find(session[:user_id]) if session[:user_id]
 	end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,5 @@
 module SessionsHelper
+	def current_user
+		@current_user = AdminUser.find(session[:AdminUser_id]) if session[:AdminUser_id]
+	end
 end
