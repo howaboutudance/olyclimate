068483bbac3e3c31b274aff778913f26f438219f
howaboutudance <howaboutudance@gmail.com>
2015-07-28 15:52:57 -0700

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 	#sessions
 	get 'login' => 'sessions#new'
 	post 'login' => 'sessions#create'
-	delete 'logout' => 'sessions#destroy'
+	get 'logout' => 'sessions#destroy'
 	
 	#oauth2
 	get 'auth/:provider/callback', to: 'sessions#create'
